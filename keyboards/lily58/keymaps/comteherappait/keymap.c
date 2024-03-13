35bfa3bfa87b4817253a144bc00b8697530b0fa5
Léon Delcroix <leondecheux@gmail.com>
2024-03-14 00:21:33 +0100

    added custom keys media/mouse

--- keyboards/lily58/keymaps/comteherappait/keymap.c
+++ keyboards/lily58/keymaps/comteherappait/keymap.c
@@ -7,6 +7,10 @@ enum layer_number {
   _ADJUST,
 };
 
+enum custom_keycodes {
+    C_COMP = SAFE_RANGE,
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [_QWERTY] = LAYOUT(
@@ -14,30 +18,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
   KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-                        KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC, KC_ENT, MO(_RAISE), KC_BSPC, KC_RGUI
+                        KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC, KC_ENT, MO(_RAISE), KC_BSPC, KC_RCTL
 ),
 
  [_LOWER] = LAYOUT(
   KC_F1  ,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                   KC_F7  ,   KC_F8,   KC_F9, KC_F10,  KC_F11,  KC_F12,
-  _______, _______, _______, _______, _______, _______,                   _______, _______, _______,_______, _______, _______,
-  KC_GRV, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD,
-  _______, _______, _______, _______, _______, _______, QK_BOOT, QK_BOOT, _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
+  _______, KC_BTN2, KC_MS_U, KC_BTN1, KC_WH_U, _______,                   _______, _______, _______, _______, _______, _______,
+  _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D,  _______,                  _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, QK_BOOT, QK_BOOT, _______, _______, _______, _______, _______, _______,
                              _______, _______, _______, _______, _______, _______,  KC_DEL, _______
 ),
 
  [_RAISE] = LAYOUT(
   KC_F1  ,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7   ,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-  _______, _______, _______, _______, _______, _______,                      _______, _______,   KC_UP, _______, KC_PSCR, _______,
+  _______, _______, _______, _______,  _______, _______,                      _______, _______,   KC_UP, _______, KC_PSCR, _______,
   _______, _______, _______, _______, _______, _______,                      _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
   _______, _______, _______, _______, _______, _______,   QK_BOOT, QK_BOOT,  _______, _______, _______, _______, _______, _______,
                              _______, _______, _______,   _______, _______,  _______,  KC_DEL, _______
 ),
 
   [_ADJUST] = LAYOUT(
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_Y   , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  C_COMP,  C_COMP, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                              _______, _______, _______, _______, _______,  _______, _______, _______
   )
 };
@@ -86,11 +90,12 @@ bool oled_task_user(void) {
 #endif // OLED_ENABLE
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  if (record->event.pressed) {
-#ifdef OLED_ENABLE
-    set_keylog(keycode, record);
-#endif
-    // set_timelog();
-  }
-  return true;
+    switch (keycode) {
+        case C_COMP:
+            if (record->event.pressed) {
+                SEND_STRING("qmk flash -kb lily58 -km comteherappait -e CONVERT_TO=promicro_rp2040" SS_TAP(X_ENTER));
+            }
+            break;
+    }
+    return true;
 }
