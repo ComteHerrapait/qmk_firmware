7a139e05685179666886c257db8678d4b01fd7f8
Léon Delcroix <l-delcroix@legallais.com>
2024-05-29 08:51:25 +0200

    swapped delete and fn on the right

--- keyboards/lily58/keymaps/comteherappait/keymap.c
+++ keyboards/lily58/keymaps/comteherappait/keymap.c
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
   KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-                        KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC, KC_ENT, MO(_RAISE), KC_BSPC, KC_RCTL
+                        KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC, KC_ENT,  KC_BSPC, MO(_RAISE), KC_RCTL
 ),
 
  [_LOWER] = LAYOUT(
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, KC_BTN2, KC_MS_U, KC_BTN1, KC_WH_U, _______,                   _______, _______, _______, _______, _______, _______,
   _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D,  _______,                  _______, _______, _______, _______, _______, _______,
   _______, _______, _______, _______, _______, _______, QK_BOOT, QK_BOOT, _______, _______, _______, _______, _______, _______,
-                             _______, _______, _______, _______, _______, _______,  KC_DEL, _______
+                             _______, _______, _______, _______, _______,  KC_DEL, _______, _______
 ),
 
  [_RAISE] = LAYOUT(
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______,  _______, _______,                      _______, _______,   KC_UP, _______, KC_PSCR, _______,
   _______, _______, _______, _______, _______, _______,                      _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
   _______, _______, _______, _______, _______, _______,   QK_BOOT, QK_BOOT,  _______, _______, _______, _______, _______, _______,
-                             _______, _______, _______,   _______, _______,  _______,  KC_DEL, _______
+                             _______, _______, _______,   _______, _______,   KC_DEL, _______, _______
 ),
 
   [_ADJUST] = LAYOUT(
